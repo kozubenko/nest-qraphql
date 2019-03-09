e3254d4336f9bc4f5e926650640fbc498219450b
andriikozubenko <andrii.kozubenko@gmail.com>
2019-03-09 11:36:14 +0200

    [FEATURE][T-0001] add basic user module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ApplicationModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(ApplicationModule);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
